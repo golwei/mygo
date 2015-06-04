801478b8fa8a40d5d63592f6e4252041f52e35a3
golwei <lizhiweimail@qq.com>
2015-06-04 19:38:09 +0800

    Update game1.go

--- game1.go
+++ game1.go
@@ -1,14 +1,13 @@
-     1 package main
-      2 
-      3 import "fmt"
-      4 
-      5 func main() {
-      6         fmt.Println("====")
-      7         for i := 0; i < 5; i++ {
-      8                 for j := 0; j < i; j++ {
-      9                         fmt.Print("*")
-     10                 }
-     11                 fmt.Println("")
-     12         }
-     13 }
-~             
+package main
+
+import "fmt"
+
+func main() {
+        fmt.Println("====")
+        for i := 0; i < 5; i++ {
+                for j := 0; j < i; j++ {
+                        fmt.Print("*")
+                }       
+                fmt.Println("")
+        }       
+}
